b70abe00fc04ab42a3e38bb235ec3dd1afd4e311
Agsergio04 <agsergio@g.educaand.es>
2026-01-26 10:25:25 +0100

    documentando el enviroment y borrado el envioment de produccion

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-// Production environment
-export const environment = {
-  production: true,
-  apiUrl: 'https://dwec-dwes-diw-proyecto-orgmedi-backend.onrender.com'
-};
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,8 @@
 // Development environment
 export const environment = {
   production: false,
+  //Para probar cosas en local descomenta el local y comenta el de render
   //apiUrl: 'http://localhost:8080'
+  //Para probar cosas en render descomenta el render y comenta el de local
   apiUrl: 'https://dwec-dwes-diw-proyecto-orgmedi-backend.onrender.com'
 };
