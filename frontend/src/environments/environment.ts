fb53bdb2e8e6e91534897cbe2c8cae79ec1d421e
Agsergio04 <agsergio@g.educaand.es>
2026-01-26 09:57:16 +0100

    Cambiando la url del envioment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 // Development environment
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  //apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://dwec-dwes-diw-proyecto-orgmedi-backend.onrender.com'
 };
