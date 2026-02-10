728a23aa677598bdc045bc7d08de9625b717a299
Agsergio04 <agsergio@g.educaand.es>
2026-02-10 10:42:51 +0100

    Creaciond del componente Hijo

---
+++ frontend/src/app/components/shared/componente-hijo/componente-hijo.ts
@@ -0,0 +1,12 @@
+import { Component,Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'componente-hijo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './componente-hijo.html',
+  styleUrls: ['./componente-hijo.scss']
+}) export class ComponenteHijo implements Input,Output {
+
+}
