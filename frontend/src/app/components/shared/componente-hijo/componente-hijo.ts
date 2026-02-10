7cce3f4c0d77ef7bcbd2b716ae5d2de2efc37619
Agsergio04 <agsergio@g.educaand.es>
2026-02-10 11:00:10 +0100

    commit para merendar

--- frontend/src/app/components/shared/componente-hijo/componente-hijo.ts
+++ frontend/src/app/components/shared/componente-hijo/componente-hijo.ts
@@ -8,5 +8,5 @@ import { CommonModule } from '@angular/common';
   templateUrl: './componente-hijo.html',
   styleUrls: ['./componente-hijo.scss']
 }) export class ComponenteHijo implements Input,Output {
-
+ 
 }
